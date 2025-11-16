6cd855b414727a8bf74f7c79a327e175e7b53996
samfisher58 <symonislam58@gmail.com>
2025-11-16 21:37:41 +0600

    added completion history collection to api endpoints that store the completed data

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
-
 app.use(cors());
 app.use(express.json());
 
@@ -31,10 +30,7 @@ async function run() {
 		const db = client.db('habit_tracker_db');
 		const habitCollection = db.collection('habits');
 		const usersCollection = db.collection('users');
-
-
-
-
+		const completionHistoryCollection = db.collection('completed task');
 
 		app.post('/users', async (req, res) => {
 			const newUser = req.body;
@@ -42,20 +38,20 @@ async function run() {
 			const email = req.body.email;
 			const query = { email: email };
 			const existingUser = await usersCollection.findOne(query);
-			if (existingUser) {res.send('user already exists, No need to add again');}
-			else {
+			if (existingUser) {
+				res.send('user already exists, No need to add again');
+			} else {
 				const result = await usersCollection.insertOne(newUser);
 				res.send(result);
 			}
 		});
 
-		app.delete('/my-habit/:id', async(req,res)=>{
+		app.delete('/my-habit/:id', async (req, res) => {
 			const id = req.params.id;
-			const query = {_id: new ObjectId(id)}
+			const query = { _id: new ObjectId(id) };
 			const result = await habitCollection.deleteOne(query);
 			res.send(result);
-
-		})
+		});
 
 		app.get('/my-habit', async (req, res) => {
 			const email = req.query.email;
@@ -79,14 +75,14 @@ async function run() {
 			res.send(result);
 		});
 		app.put('/publicHabits/:id', async (req, res) => {
-			const id = req.params.id
-			const data = req.body
-			const query = {_id: new ObjectId(id) }
-			const filter = query
+			const id = req.params.id;
+			const data = req.body;
+			const query = { _id: new ObjectId(id) };
+			const filter = query;
 			const update = {
-				$set: data
-			}
-			const result = await habitCollection.updateOne(filter,update);
+				$set: data,
+			};
+			const result = await habitCollection.updateOne(filter, update);
 			res.send(result);
 		});
 		app.get('/habits/:id', async (req, res) => {
@@ -95,35 +91,39 @@ async function run() {
 			const result = await habitCollection.findOne(query);
 			res.send(result);
 		});
+
 		app.get('/publicHabits/:id', async (req, res) => {
 			const id = req.params.id;
 			const query = { _id: new ObjectId(id) };
 			const result = await habitCollection.findOne(query);
+
+			if (!result) {
+				return res.status(404).send({ error: 'Habit not found' });
+			}
+
 			res.send(result);
 		});
-
 
 		app.patch('/habits/:id/complete', async (req, res) => {
 			const id = req.params.id;
 			const query = { _id: new ObjectId(id) };
-			const habit = await habitCollection.findOne(query);
-			
 
-			
+			const habit = await habitCollection.findOne(query);
+			if (!habit) {
+				return res.status(404).send({ message: 'Habit not found' });
+			}
+
 			const today = new Date();
 			const todayStr = today.toLocaleDateString('en-GB').split('/').join('-');
 
 			const completionHistory = habit.progress?.completionHistory || [];
 
-			
 			if (completionHistory.includes(todayStr)) {
 				return res.send({ message: 'Already completed for today' });
 			}
 
-			
 			completionHistory.push(todayStr);
 
-			
 			const sortedDates = completionHistory.sort(
 				(a, b) =>
 					new Date(b.split('-').reverse().join('-')) -
@@ -137,6 +137,7 @@ async function run() {
 				);
 				const curr = new Date(sortedDates[i].split('-').reverse().join('-'));
 				const diffDays = Math.floor((prev - curr) / (1000 * 60 * 60 * 24));
+
 				if (diffDays === 1) currentStreak++;
 				else break;
 			}
@@ -155,21 +156,26 @@ async function run() {
 			};
 
 			const result = await habitCollection.updateOne(query, updateDoc);
+
+			const completionRecord = {
+				habitId: id,
+				habitName: habit.user?.name,
+				userEmail: habit.user?.email,
+				date: todayStr,
+				completedAt: new Date(),
+			};
+
+			await completionHistoryCollection.insertOne(completionRecord);
+
 			res.send({
 				message: 'Habit marked complete',
 				currentStreak,
 				bestStreak,
 				completionHistory,
 				result,
+				savedTo: 'completionHistory collection',
 			});
 		});
-
-
-		
-
-
-
-
 
 		app.post('/publicHabits', async (req, res) => {
 			const newHabit = req.body;
